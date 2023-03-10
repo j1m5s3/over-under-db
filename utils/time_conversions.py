50579ed35b7681ba7a8d44865ddca63fd18449d9
j1m5s3 <james.lynch.uml@gmail.com>
2023-03-09 21:04:05 -0600

    Added mongo interface and time conversions

---
+++ utils/time_conversions.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+
+def convert_to_datetime(date_string):
+    return datetime.strptime(date_string, '%Y-%m-%d')
+
+
+def convert_to_date(date_string):
+    return convert_to_datetime(date_string).date()
+
+
+def convert_to_timestamp(date_string):
+    return convert_to_datetime(date_string).timestamp()
+
+
+def convert_to_datetime_from_timestamp(timestamp):
+    return datetime.fromtimestamp(timestamp)
+
+
+def convert_to_date_from_timestamp(timestamp):
+    return convert_to_datetime_from_timestamp(timestamp).date()
+
+
+def convert_to_timestamp_from_datetime(date_time):
+    return date_time.timestamp()
+
+
+def convert_datetime_to_unix_timestamp(date_time):
+    return int(date_time.timestamp())
+
+
+def convert_date_string_to_unix_timestamp(date_string):
+    return convert_datetime_to_unix_timestamp(convert_to_datetime(date_string))
